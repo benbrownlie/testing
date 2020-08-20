a517f793f5949a147584c77eeaf74f1270d17026
Benjamin Brownlie <benjibspam@gmail.com>
2020-08-20 12:53:52 -0500

    Practice
    Practicing

--- Game.cs
+++ Game.cs
@@ -8,6 +8,36 @@ namespace HelloWorld
     {
         public void Run()
         {
+            //Initial greeting and game ready dialogue
+            string greeting = "Hello Survivor! Are you ready? ";
+            string gameReady = "Welcome to the Graveyard";
+            string gameNotReady = "Goodbye";
+
+            bool gameStart = false;
+            Console.WriteLine(greeting + gameStart);
+            Console.WriteLine("Press 1. for Ready");
+            Console.WriteLine("Press 2. to Leave");
+            char input = Console.ReadKey().KeyChar;
+
+            if (input == '1') ;
+            {
+                Console.WriteLine("\n" + gameReady);
+
+
+            }
+
+            
+            else if (input == '2') ;
+            {
+                Console.WriteLine("\n" + gameNotReady);
+
+
+
+            }
+
+            
+
+
 
         }
     }
